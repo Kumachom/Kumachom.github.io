e70fbc20da7f76300d7df47e672ce86dafe69af8
kumachom <kumachan.seashine@gmail.com>
2026-03-08 19:28:54 +0900

    削除ボタン作成中

--- index1.js
+++ index1.js
@@ -1,12 +1,12 @@
 //テキスト追加関数
 function addtext(){
     const category = document.getElementById("category").value;
-    const select = document.getElementById("select").value.trim();
+    const select = document.getElementById("inputText").value.trim();
 
     if(select){
         const displayArea = document.getElementById("textList");
         displayArea.innerHTML += category + ":" + select + "<br>";  //カテゴリを追加
-        document.getElementById("select").value = "";
+        document.getElementById("inputText").value = "";
     }
 
 
@@ -15,6 +15,12 @@ function addtext(){
 //リセット関数
 function resettext(){
     document.getElementById("textList").innerHTML = "";
-    document.getElementById("select").value = "";
+    document.getElementById("inputText").value = "";
     document.getElementById("category").selectedIndex = 0;
+}
+
+//テキスト削除関数
+function deletetext(){
+    const Item = GamepadButton.parentElement;
+    Item.remove();
 }
