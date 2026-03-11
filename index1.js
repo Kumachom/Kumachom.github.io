35d3058c56f2e54974fffbd1117f9169d81ab6ed
kumachom <kumachan.seashine@gmail.com>
2026-03-12 01:49:10 +0900

    カレンダー追加

--- index1.js
+++ index1.js
@@ -2,10 +2,16 @@
 function addtext(){
     const category = document.getElementById("category").value;
     const select = document.getElementById("inputText").value.trim();
+    const date = document.getElementById("date").value;
 
     if(select){
         const displayArea = document.getElementById("textList");
-        displayArea.innerHTML += category + ":" + select + "<br>";  //カテゴリを追加
+
+        const item = document.createElement("div");     //1行作る
+        item.innerHTML = date + "|" +category + ":" + select + ' <button onclick = "deletetext(this)">削除</button>';
+        
+        displayArea.appendChild(item); 
+
         document.getElementById("inputText").value = "";
     }
 
@@ -17,10 +23,12 @@ function resettext(){
     document.getElementById("textList").innerHTML = "";
     document.getElementById("inputText").value = "";
     document.getElementById("category").selectedIndex = 0;
+    document.getElementById("date").value = "";
 }
 
 //テキスト削除関数
-function deletetext(){
-    const Item = GamepadButton.parentElement;
-    Item.remove();
+function deletetext(button){
+    const item = button.parentElement;
+    item.remove();
 }
+
